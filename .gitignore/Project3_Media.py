cbe51fc7079c5cae4cc6b5834c0dc238bb0c93fa
stfnylim <stfnylim@gmail.com>
2018-05-02 15:43:16 -0700

    added methods to media for member class

--- .gitignore/Project3_Media.py
+++ .gitignore/Project3_Media.py
@@ -29,27 +29,37 @@ class Media: # Superclass; Subclasses: Books and Videos
         self.publisher = publisher
 
 class Books(Media): # Subclass of Media
-    pass
+    def __init__(self, title, author, publisher,num_pages):
+        super().__init__(title, author, publisher) # inherit attributes from Media
+        self.num_pages = num_pages # add attribute for number of pages
+        self.num_books += 1 # increment to keep track of number of books
+    def __repr__(self): # return information of the class books
+        return "Book--> {} page book {} written by {}".format(self.num_pages,self.title, self.author)
 
 class Videos(Media): #Subclass of Media
-    pass
+    def __init__(self, title, author, publisher, run_time):
+        super().__init__(title, author, publisher) # inherit attributes from Media
+        self.run_time = run_time # add attribute for run time
+        self.num_videos += 1 # increment to keep track of number of videos
+    def __repr__(self): # return information of the class video
+        return "Video --> {} min video {} created by {}".format(self.run_time,self.title, self.author)
 
 class Members: # Separate class
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,name): # define attributes
+        self.name = name # attribute for member name
         self.bookCheckOutList = []
-        self.num_members = 0
+        self.num_members = 0 # initialize the number of members
         self.list_members = []
-        self.num_books_checked = 0
+        self.num_books_checked = 0 # initialize the number of books checked out
     def addMembers(self):
-        self.num_members += 1
+        self.num_members += 1 # increment to keep track of number of members
 
     def checkOut(self,a_media):
 
-        self.num_books_checked += 1
-        print("{} has checked out: {}".format(self.name, a_media))
+        self.num_books_checked += 1 # increment for number of books checked out
+        print("{} has checked out: {}".format(self.name, a_media)) # output the member and media info
 
     def checkIn(self, a_media):
 
-        self.num_books_checked -= 1
-        print("{} checked in: {}".format(self.name, a_media))
+        self.num_books_checked -= 1 # decrement for number of books checked out
+        print("{} checked in: {}".format(self.name, a_media)) # output the member and media info
