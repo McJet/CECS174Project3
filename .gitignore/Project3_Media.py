b6d144a05ad72fc1c74caffc4b9b09f7bf664ba9
Michael M <george_maalouf2011@hotmail.com>
2018-05-04 12:07:49 -0700

    adding Stephanie's work

--- .gitignore/Project3_Media.py
+++ .gitignore/Project3_Media.py
@@ -61,14 +61,47 @@ class Videos(Media): #Subclass of Media
         self.runTime = runTime
 
 class Members: # Separate class
+    def __init__(self, name):  # define attributes
+        self.name = name  # attribute for member name
+        self.bookCheckOutList = []
+        self.num_members = 0  # initialize the number of members
+        self.list_members = []
+        self.num_books_checked = 0  # initialize the number of books checked out
+
+    def addMembers(self):
+        self.num_members += 1  # increment to keep track of number of members
+
+    def checkOut(self, a_media):
+        self.bookCheckOutList.append(a_media)
+        #### FIXME: Have to figure out how to prevent checking out the same book ####
+
+        if self.num_books_checked > 1:  # create limit to num books checked out (2)
+            print(
+                "{} reached the maximum number(2) of borrowed items, so can't check out:{}".format(self.name, a_media))
+        else:
+            self.num_books_checked += 1  # increment for number of books checked out
+            print("{} has checked out: {}".format(self.name, a_media))  # output the member and media info
+
+    def printCheckedOutItems(self):  # shows all the checked out items
+        print('*' * 40)
+        print('The following items are checked out of the library:')
+        for item in self.bookCheckOutList:
+            print(item)
+
+    def checkIn(self, a_media):
+
+        self.num_books_checked -= 1  # decrement for number of books checked out
+        print("{} checked in: {}".format(self.name, a_media))  # output the member and media info
 
 
-
-
-
-
-    
-    pass
-
-
-
+a_book1 = Videos("Hello", "Steph Lim", "Penguin", 100)
+a_book2 = Books("Bye", "Steph", "Penguin", 200)
+a_book3 = Videos("Hi", "Steph Lim", "Penguin", 300)
+a_book4 = Videos("Sup", "Steph Lim", "Penguin", 300)
+student = Members('Lim')
+student.checkOut(a_book1)
+student.checkOut(a_book2)
+student.checkOut(a_book3)
+student.checkOut(a_book4)
+student.printCheckedOutItems()
+student2 = Members('Park')
